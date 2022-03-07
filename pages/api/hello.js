1c0389d15156d73d9637cd1e56eda03b59b01bba
samuelghill <samuelgarretthill@gmail.com>
2022-03-07 11:16:45 -0500

    added data tables from stepzen endpoint

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,95 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getProviders } from '../../lib/api'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async (req, res) => {
+
+  let address
+  if(req?.query?.address) {
+    address = req.query.address
+  } else {
+    address = process.env.ADDRESS
+  }
+
+  const data = await getProviders(
+    process.env.STEPZEN_API_URL,
+    process.env.STEPZEN_API_KEY,
+    `
+    query DefaultProvider($address: String!, $etherscan_apikey: Secret!, $infura_app_id: Secret!, $moralis_apikey: Secret!) {
+      moralis_erc20(
+        address: $address
+        moralis_apikey: $moralis_apikey
+      ) {
+        name
+        symbol
+        thumbnail
+        balance
+        decimals
+        logo
+      }
+      moralis_nft(
+        address: "0xd07dc4262BCDbf85190C01c996b4C06a461d2430"
+        moralis_apikey: $moralis_apikey
+        tokenId: "140082"
+      ) {
+        tokenId
+        tokenUri
+        tokenAddress
+        syncedAt
+        symbol
+        name
+        metadata
+        contractType
+        amount
+      }
+      infura_gas_price(infura_app_id: $infura_app_id) {
+        id
+        jsonrpc
+        result
+      }  
+      etherscan_logs(etherscan_apikey: $etherscan_apikey) {
+        message
+        status
+        result {
+          address
+          blockNumber
+          data
+          gasPrice
+          gasUsed
+          logIndex
+          timeStamp
+          transactionHash
+          transaction(etherscan_apikey: $etherscan_apikey) {
+            value
+            v
+            type
+            transactionIndex
+            to
+            s
+            r
+            nonce
+            maxPriorityFeePerGas
+            maxFeePerGas
+            input
+            hash
+            gas
+            from
+            chainId
+            gasPrice
+            blockNumber
+            blockHash
+          }
+        }
+      }
+    }
+  `,
+    {
+      variables: {
+        address: address,
+        etherscan_apikey: process.env.ETHERSCAN_API_KEY,
+        moralis_apikey: process.env.MORALIS_API_KEY,
+        infura_app_id: process.env.INFURA_APP_ID
+      },
+    }
+  );
+
+  res.status(200).json({ data: data })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import detectEthereumProvider from '@metamask/detect-provider';
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import detectEthereumProvider from "@metamask/detect-provider";
 
-export default function Home() {
-  const [provider, setProvider] = useState('');
-  const [publicAddress, setPublicAddress] = useState('');
+import { AddStr, Reverse } from "../utils/functions";
+
+export default function Home(props) {
+  const [provider, setProvider] = useState("");
+  const [publicAddress, setPublicAddress] = useState("");
+
+  // Get the data from StepZen API
+  const [moralisTokenBalances, setMoralisTokenBalances] = useState("");
+  const [moralisNFT, setMoralisNFT] = useState("");
+  const [infuraGasPrices, setInfuraGasPrices] = useState("");
+  const [etherscanLogs, setEtherscanLogs] = useState([]);
 
   useEffect(() => {
     async function setEthereumProvider() {
@@ -20,15 +28,35 @@ export default function Home() {
 
   const onConnect = async () => {
     if (provider) {
-      console.log('Ethereum successfully detected!');
+      console.log("Ethereum successfully detected!");
 
       const [ethereumPublicAddress] = await provider.request({
-        method: 'eth_requestAccounts',
+        method: "eth_requestAccounts",
       });
+      if (ethereumPublicAddress) {
+        setPublicAddress(ethereumPublicAddress);
+        // Fetch data from external API
+        const res = await fetch(`/api/hello?address=${ethereumPublicAddress}`)
+        let data = await res.json()
+        data = data.data
 
-      if (ethereumPublicAddress) setPublicAddress(ethereumPublicAddress);
+        console.log("data", data);
+        setMoralisTokenBalances(data.moralis_erc20);
+
+        let metadata = JSON.parse(data.moralis_nft.metadata);
+        setMoralisNFT(`https://ipfs.io/${metadata.image.slice(7)}`);
+
+        setInfuraGasPrices(data.infura_gas_price);
+
+        // If a transaction was linked to the ethereum log
+        data.etherscan_logs.result.forEach(function (element) {
+          if (element.transaction.value) {
+            setEtherscanLogs((etherscanLogs) => [...etherscanLogs, element]);
+          }
+        });
+      }
     } else {
-      console.error('Please install MetaMask!', error);
+      console.error("Please install MetaMask!", error);
     }
   };
 
@@ -36,13 +64,12 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>StepZen web3 app</title>
-        <meta name='description' content='StepZen web3 app' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="StepZen web3 app" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to StepZen web3</h1>
-
         {publicAddress ? (
           <p className={styles.description}>
             Your public address
@@ -56,28 +83,126 @@ export default function Home() {
               </button>
             ) : (
               <a
-                href='https://metamask.io/download/'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://metamask.io/download/"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 Install MetaMask
               </a>
             )}
           </div>
         )}
+        {moralisNFT ? (
+          <>
+            <img style={{ maxWidth: "100px" }} src={moralisNFT} alt="elon" />
+            <h5 style={{ margin: "5px", color: "grey" }}>
+              <em>source: moralis</em>
+            </h5>
+          </>
+        ) : null}
+        {etherscanLogs.length > 0 ? (
+          <>
+            <h3 style={{ margin: "40px 0 0 0" }}>
+              Hash Logs with Transaction Details
+            </h3>
+            <h5 style={{ margin: "5px", color: "grey" }}>
+              <em>source: etherscan</em>
+            </h5>
+            <table style={styles.table}>
+              <tbody style={styles.tbody}>
+                <tr style={{ borderBottom: "2px solid black" }}>
+                  <td>Token</td>
+                  <td>Balance</td>
+                  <td>Gas Used</td>
+                  <td>Transaction Hash</td>
+                  <td>Transaction Input</td>
+                </tr>
+                {etherscanLogs.map((log, index) => (
+                  <tr key={index}>
+                    <td>{log.address}</td>
+                    <td>{log.blockNumber}</td>
+                    <td>{log.gasUsed}</td>
+                    <td>{log.transaction.hash.substring(0, 20)}...</td>
+                    <td>{log.transaction.input.substring(0, 20)}...</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        ) : null}
+        {moralisTokenBalances ? (
+          <>
+            <h3 style={{ margin: "40px 0 0 0" }}>Your Token Balance</h3>
+            <h5 style={{ margin: "5px", color: "grey" }}>
+              <em>source: moralis</em>
+            </h5>
+            <table style={styles.table}>
+              <tbody style={styles.tbody}>
+                <tr style={{ borderBottom: "2px solid black" }}>
+                  <td>Token</td>
+                  <td>Balance</td>
+                </tr>
+                {moralisTokenBalances.map((token_balances, index) => (
+                  <tr key={index}>
+                    <td>{token_balances.name}</td>
+                    <td>
+                      {Reverse(
+                        AddStr(
+                          token_balances.balance,
+                          Number(token_balances.decimals),
+                          "."
+                        )
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        ) : null}
+        {infuraGasPrices ? (
+          <>
+            <h3 style={{ margin: "40px 0 0 0" }}>Current Gas Price</h3>
+            <h5 style={{ margin: "5px", color: "grey" }}>
+              <em>source: infura</em>
+            </h5>
+            <table style={styles.table}>
+              <tbody style={styles.tbody}>
+                <tr style={{ borderBottom: "2px solid black" }}>
+                  <td>Price Type</td>
+                  <td>Current</td>
+                </tr>
+                <tr>
+                  <td>wei (hexidecimal)</td>
+                  <td>{infuraGasPrices.result}</td>
+                </tr>
+                <tr>
+                  <td>wei</td>
+                  <td>{parseInt(infuraGasPrices.result, 16)}</td>
+                </tr>
+                <tr>
+                  <td>ETH</td>
+                  <td>
+                    {parseInt(infuraGasPrices.result, 16) / 1000000000000000000}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </>
+        ) : null}
       </main>
 
       <footer className={styles.footer}>
         <a
-          href='https://stepzen.com?utm_source=stepzen-examples&utm_medium=default-template&utm_campaign=stepzen-examples'
-          target='_blank'
-          rel='noopener noreferrer'
+          href="https://stepzen.com?utm_source=stepzen-examples&utm_medium=default-template&utm_campaign=stepzen-examples"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image
-              src='/stepzen.svg'
-              alt='StepZen Logo'
+              src="/stepzen.svg"
+              alt="StepZen Logo"
               width={100}
               height={25}
             />
